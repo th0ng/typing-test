14f0992bcc808c9fc269b21e2d21a747521f9d55
Thongzxje <thonghoangdanh@gmail.com>
2023-01-02 20:16:20 +0200

    invalid keys

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { Header } from './components';
+import { Header } from "./components";
 
 const App = () => {
+  const invalidKeys =
+    "F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 Escape Tab CapsLock Shift Control Alt Meta ArrowLeft ArrowRight ArrowDown ArrowUp Enter".split(
+      " "
+    );
+
   return (
     <>
       <Header />
       <h1>Typing test</h1>
     </>
   );
-}
+};
 
 export default App;
